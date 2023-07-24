3d686e5c20043fd7d90cf95a6643cebcf3311524
Alok <alok.s@dataman.in>
2023-07-24 11:52:35 +0530

    24-July-2023

--- src/PracticeComponents/BetterWays-ReactJs.jsx
+++ src/PracticeComponents/BetterWays-ReactJs.jsx
@@ -7,8 +7,8 @@
 				data: { Mobile: '9140198644' },
 				url: 'http://license.dataman.in/webservice2.asmx/GetOTPForGrahaakField',
 			};
-			const apiResp = await axios(apiOptions);
+			const apiResp = await axios(apiOptions)
 			console.log(apiResp.data);
 		};
 		postApiData();
-	}, []);
+	}, []); 
